3817d4d0a1a08354f512ae0ded87a019897a5154
Thomas Grainger <tom@procensus.com>
2017-12-12 19:52:18 +0000

    switch to airbnb 2 spaces

--- chai-hiff.js
+++ chai-hiff.js
@@ -2,50 +2,50 @@ var colors = require('colors/safe');
 var hiff = require('hiff');
 
 function processChange(change) {
-    return colors.reset('\t') + 'In node ' + change.before.parentPath + ':\n\t' + change.message;
+  return colors.reset('\t') + 'In node ' + change.before.parentPath + ':\n\t' + change.message;
 }
 
 function obj(chaiContext) {
-    return chaiContext._obj; // eslint-disable-line no-underscore-dangle
+  return chaiContext._obj; // eslint-disable-line no-underscore-dangle
 }
 
 function createMethod(flag, hiffCheck) {
-    return function getHiffMethod(_super) {
-        return function hiffMethod() {
-            var meth = flag(this, 'hiff') ? hiffCheck : _super;
-            meth.apply(this, arguments);
-        };
+  return function getHiffMethod(_super) {
+    return function hiffMethod() {
+      var meth = flag(this, 'hiff') ? hiffCheck : _super;
+      meth.apply(this, arguments);
     };
+  };
 }
 
 function hiffEqual(expected, options) {
-    var actual = obj(this);
-    var result = hiff.compare(expected, actual, options);
-    var changes = result.different ? result.changes.map(processChange).join('\n') : '';
+  var actual = obj(this);
+  var result = hiff.compare(expected, actual, options);
+  var changes = result.different ? result.changes.map(processChange).join('\n') : '';
 
-    this.assert(
-        !result.different,
-        'expected html to match, but there were these changes: \n\n' + changes,
-        'expected #{act} to not match #{exp}',
-        expected,
-        actual,
-        true
-    );
+  this.assert(
+    !result.different,
+    'expected html to match, but there were these changes: \n\n' + changes,
+    'expected #{act} to not match #{exp}',
+    expected,
+    actual,
+    true
+  );
 }
 
 module.exports = function chaiHiff(chai, utils) {
-    var Assertion = chai.Assertion;
-    var flag = utils.flag;
-    var equal = createMethod(flag, hiffEqual);
+  var Assertion = chai.Assertion;
+  var flag = utils.flag;
+  var equal = createMethod(flag, hiffEqual);
 
-    Assertion.addMethod('hiffEqual', hiffEqual);
-    Assertion.addMethod('hiffEquals', hiffEqual);
-    Assertion.addMethod('hiffEq', hiffEqual);
-    Assertion.addProperty('hiff', function hiffProp() {
-        flag(this, 'hiff', true);
-    });
+  Assertion.addMethod('hiffEqual', hiffEqual);
+  Assertion.addMethod('hiffEquals', hiffEqual);
+  Assertion.addMethod('hiffEq', hiffEqual);
+  Assertion.addProperty('hiff', function hiffProp() {
+    flag(this, 'hiff', true);
+  });
 
-    Assertion.overwriteMethod('equal', equal);
-    Assertion.overwriteMethod('equals', equal);
-    Assertion.overwriteMethod('eq', equal);
+  Assertion.overwriteMethod('equal', equal);
+  Assertion.overwriteMethod('equals', equal);
+  Assertion.overwriteMethod('eq', equal);
 };
--- test/chai-hiff.test.js
+++ test/chai-hiff.test.js
@@ -4,47 +4,47 @@ var expect = chai.expect;
 chai.use(require('../'));
 
 describe('chai-hiff', function () {
-    describe('.hiffEqual()', function () {
-        it('should match some differing but equivalent HTML', function () {
-            expect(function () {
-                expect('<input name="my-input" value="stuff">').to.hiffEqual('<input value="stuff" name="my-input">');
-            }).not.to.throw();
-        });
-
-        it('should not match some non-equivalent HTML', function () {
-            expect(function () {
-                expect('<input name="my-input" value="stuff">').to.hiffEqual('<div class="a-div"></div>');
-            }).to.throw('expected html to match, but there were these changes');
-        });
+  describe('.hiffEqual()', function () {
+    it('should match some differing but equivalent HTML', function () {
+      expect(function () {
+        expect('<input name="my-input" value="stuff">').to.hiffEqual('<input value="stuff" name="my-input">');
+      }).not.to.throw();
     });
 
-    describe('.hiff.equal()', function () {
-        it('should match some differing but equivalent HTML', function () {
-            expect(function () {
-                expect('<input name="my-input" value="stuff">').to.hiff.equal('<input value="stuff" name="my-input">');
-            }).not.to.throw();
-        });
+    it('should not match some non-equivalent HTML', function () {
+      expect(function () {
+        expect('<input name="my-input" value="stuff">').to.hiffEqual('<div class="a-div"></div>');
+      }).to.throw('expected html to match, but there were these changes');
+    });
+  });
 
-        it('should not match some non-equivalent HTML', function () {
-            expect(function () {
-                expect('<input name="my-input" value="stuff">').to.hiff.equal('<div class="a-div"></div>');
-            }).to.throw('expected html to match, but there were these changes');
-        });
+  describe('.hiff.equal()', function () {
+    it('should match some differing but equivalent HTML', function () {
+      expect(function () {
+        expect('<input name="my-input" value="stuff">').to.hiff.equal('<input value="stuff" name="my-input">');
+      }).not.to.throw();
     });
 
-    describe('.not.hiff.equal()', function () {
-        it('should not match some differing but equivalent HTML', function () {
-            var actual = '<input name="my-input" value="stuff">';
-            var expected = '<input name="my-input" value="stuff">';
-            expect(function () {
-                expect(actual).not.to.hiff.equal(expected);
-            }).to.throw("expected '" + actual + "' to not match '" + expected + "'");
-        });
-
-        it('should match some non-equivalent HTML', function () {
-            expect(function () {
-                expect('<input name="my-input" value="stuff">').not.to.hiff.equal('<div class="a-div"></div>');
-            }).not.to.throw();
-        });
+    it('should not match some non-equivalent HTML', function () {
+      expect(function () {
+        expect('<input name="my-input" value="stuff">').to.hiff.equal('<div class="a-div"></div>');
+      }).to.throw('expected html to match, but there were these changes');
     });
+  });
+
+  describe('.not.hiff.equal()', function () {
+    it('should not match some differing but equivalent HTML', function () {
+      var actual = '<input name="my-input" value="stuff">';
+      var expected = '<input name="my-input" value="stuff">';
+      expect(function () {
+        expect(actual).not.to.hiff.equal(expected);
+      }).to.throw("expected '" + actual + "' to not match '" + expected + "'");
+    });
+
+    it('should match some non-equivalent HTML', function () {
+      expect(function () {
+        expect('<input name="my-input" value="stuff">').not.to.hiff.equal('<div class="a-div"></div>');
+      }).not.to.throw();
+    });
+  });
 });
